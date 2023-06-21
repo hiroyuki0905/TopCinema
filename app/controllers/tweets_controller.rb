0906c1cdbc7ea4cd7d6fc1c378df3dbb7b112c64
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-06-22 00:47:02 +0900

    トップページ実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,24 @@
 class TweetsController < ApplicationController
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path # ランキング投稿後にトップページにリダイレクトする
+    else
+      render :new
+    end
+  end
 
   def index
     @tweets = Tweet.all
   end
 
-  def new
-    @tweet = Tweet.new
-  end
+  private
 
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text, :rank)
+  end
 end
