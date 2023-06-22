2297dab69f7e0cf8dea71724368bfe50c2502ced
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-06-22 21:24:46 +0900

    deviseのビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, skip: [:registrations]  # 新規登録ルートをスキップする
   root to: 'tweets#index'
   resources :tweets, only: [:index, :new]
+  get '/service.html', to: 'pages#service_html'
+  get '/users/new', to: 'devise/registrations#new', as: 'new_user_registration'  # 新規登録ルートを追加
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
