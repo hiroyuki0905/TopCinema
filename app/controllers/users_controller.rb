42f7af9f595ea910ccf700feb6e02156a491ebce
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-07-06 23:24:02 +0900

    userコントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   def show
-    user = User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id])
 
-    if user
-      @nickname = user.nickname
-      @tweets = user.tweets
+    if @user
+      @nickname = @user.nickname
+      @tweets = @user.tweets
     else
-      redirect_to new_user_session_path, notice: 'ユーザーが見つかりません'
+      redirect_to new_user_registration_path, notice: 'ユーザーが見つかりません'
     end
   end
 end
