6c161ae084e93a29ff201fb8c000d8e23deee0aa
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-07-17 21:38:15 +0900

    トップページ変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by(id: params[:id])
-
-    if @user
-      @nickname = @user.nickname
-      @tweets = @user.tweets
-    else
-      redirect_to new_user_registration_path, notice: 'ユーザーが見つかりません'
-    end
+  def index
+    @users = User.all
   end
-end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+  
+end
