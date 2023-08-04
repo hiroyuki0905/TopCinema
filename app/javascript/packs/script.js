25f11aeb9f0436f56d37bfe42b6498f3cf7e1bb2
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-08-04 22:16:07 +0900

    アニメーションを追加

---
+++ app/javascript/packs/script.js
@@ -0,0 +1,66 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Car {
+  constructor(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.width = 190;
+    this.height = 60;
+  }
+
+  draw() {
+    ctx.fillStyle = "gray"; // 車の色を指定（灰色）
+    ctx.beginPath(); // 新しいパスを開始
+    ctx.moveTo(this.x - 50, this.y + this.height - 10); // パスの開始位置を指定（左下）
+    ctx.lineTo(this.x + this.width - 50, this.y + this.height - 10); // パスを引いて線を描画（下辺）
+    ctx.lineTo(this.x + this.width, this.y + this.height - 10); // パスを引いて線を描画（右上）
+    ctx.lineTo(this.x + this.width, this.y + 35); // パスを引いて線を描画（右下）
+    ctx.lineTo(this.x + this.width - 200, this.y - 30); // パスを引いて線を描画（斜め上左）
+    ctx.lineTo(this.x + 10, this.y); // パスを引いて線を描画（上辺）
+    ctx.closePath(); // パスを閉じる
+    ctx.fill(); // パス内を塗りつぶす
+
+    ctx.fillStyle = "black"; // タイヤの色を指定（黒色）
+
+    // タイヤ1
+    ctx.beginPath(); // 新しいパスを開始
+    ctx.arc(this.x + 5, this.y + 50, 15, 0, Math.PI * 2); // 円を描画（タイヤ1）
+    ctx.closePath(); // パスを閉じる
+    ctx.fill(); // パス内を塗りつぶす
+
+    // タイヤ2
+    ctx.beginPath(); // 新しいパスを開始
+    ctx.arc(this.x + this.width - 40, this.y + 50, 15, 0, Math.PI * 2); // 円を描画（タイヤ2）
+    ctx.closePath(); // パスを閉じる
+    ctx.fill(); // パス内を塗りつぶす
+
+    // 窓を描画（黒色）
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x + 20, this.y + 9, this.width - 100, 15); // 窓の位置とサイズを指定
+  }
+
+  update() {
+    this.x += this.speed;
+    if (this.x > canvas.width) {
+      this.x = -this.width;
+    }
+  }
+}
+
+const car = new Car(-150, canvas.height - 100, 10); // スピード調整
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  car.draw();
+  car.update();
+
+  requestAnimationFrame(animate);
+}
+
+animate();
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'tweets#index'
   devise_for :users
-  resources :tweets do
-    resources :comments, only: :create
-    collection do
-      get 'search'
-    end
-  end
-  resources :users, only: [:index, :new,]
+  root to: 'tweets#index'
+  resources :tweets
 end
