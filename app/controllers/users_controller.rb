1b6c6edc6a037d312c2922519d99b1d968ff67ce
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-08-12 00:55:21 +0900

    プルダウン修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
   def show
-  end
-
-  def edit
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets
   end
 end
