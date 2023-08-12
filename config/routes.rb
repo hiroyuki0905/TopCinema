86fdeae90aeac439f2b9037f22a2149a99df5de6
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-08-13 01:03:47 +0900

    マイページ詳細ページ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
-  resources :tweets
+  resources :tweets, only: [:index, :new, :create, :destroy]
+
+  resources :users, only: [:show]
+
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
