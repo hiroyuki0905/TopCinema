1d8eaac7553cc9cc8871654a561e00647170448c
hiroyuki0905 <yuxinggaoqiao914@gmail.com>
2023-08-20 00:10:52 +0900

    ロゴ表示変更

--- db/migrate/20230614132138_create_tweets.rb
+++ db/migrate/20230614132138_create_tweets.rb
@@ -1,6 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
+      t.references :user, foreign_key: true
       t.string :name
       t.string :text
       t.text :image
